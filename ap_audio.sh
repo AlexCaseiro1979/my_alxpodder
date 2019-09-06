9e3c239b33de1f506b529e0a2fc80c504a3ccc9c
AlexCaseiro1979 <alex.caseiro@gmail.com>
2019-09-06 17:24:35 +0200

    Fri 06 Sep 2019 05:24:35 PM CEST

--- ap_audio.sh
+++ ap_audio.sh
@@ -11,15 +11,19 @@ NC='\033[0m'
 # Make script crontab friendly:
 cd $(dirname $0)
 
+# Some strings
+extm3u=.m3u
+extmp3=.mp3
+seprdr=_
+
 # if the program is called to clean a previous run which went wrong:
-if [ "$1" = "clean" ]; then
-	cat podcast.log >> temp_pc.log ; sort temp_pc.log | uniq > podcast.log ; rm temp_pc.log
-	rm audio.mp3 parse_enclosure2.xsl
+if [[ "$1" = "clean" ]]; then
+cat podcast.log >> temp_pc.log ; sort temp_pc.log | uniq > podcast.log ; rm temp_pc.log
+rm audio.mp3 parse_enclosure2.xsl
 
 else
 
 # write the parse_enclosure2.xsl file
-
 cat > parse_enclosure2.xsl << _EOF_
 <?xml version="1.0"?>
 <stylesheet version="1.0"
@@ -34,54 +38,66 @@ cat > parse_enclosure2.xsl << _EOF_
 </stylesheet>
 _EOF_
 
-# datadir is the directory you want podcasts saved to, create datadir if necessary:
+# datadir is the directory you want podcasts saved to
 datadir=$(date +%Y-%m-%d_%H-%M)
 mkdir -p $datadir
 
-# Delete any temp_pc file:
+# Delete any leftover temp_pc file:
 rm -f temp_pc.log
 
-# Read the $1 file and wget any url not already in the podcast.log file, and rename the downloaded file:
+# Read the $1 file and wget any url not already in the podcast.log file
+# and rename the downloaded file:
 countlines=1
-while read podcast
-	do
-		rem=$(( $countlines % 2 ))
-		if [ $rem -eq 1 ]; then
-		podname=$podcast
-		echo -e "${YELLOW}$podname${NC}"
-		else
-		file=$(xsltproc parse_enclosure2.xsl $podcast 2> /dev/null || wget -q $podcast -O - | tr '\r' '\n' | tr \' \" | sed -n 's/.*url="\([^"]*\)".*/\1/p')
-			for url in $file
-				do
-				echo $url >> temp_pc.log
-				if [ "$2" != "no-download" ]
-					then
-					if ! grep "$url" podcast.log > /dev/null
-						then
-						#echo -e "${GREEN}\t$url${NC}"
-						wget -q "$url" -O audio.mp3
-						poddate=$(stat -c "%y" "audio.mp3"|awk '{print $1"_"$2}'|sed 's/\..*$//'|sed 's/:/-/'|sed 's/:/-/')
-						extmp3=.mp3
-						seprdr=_
-						newname=$podname$seprdr$poddate$extmp3
-						n=1
-						while [ -f $(pwd)/$datadir/$newname  ]
-							do
-							newname=$podname$seprdr$poddate$n$extmp3
-							n=$(expr $n + 1)
-							done
-                                                if [ \! -s audio.mp3 ];then
-                                                    rm "$(pwd)/audio.mp3"
-                                                else
-						    echo -e "${GREEN}\t$newname${NC}"
-						    mv "$(pwd)/audio.mp3" "$(pwd)/$datadir/$newname"
-                                                fi
-					fi
-				fi
-				done
-		fi
-		countlines=$(expr $countlines + 1)
-	done < $1
+while read podcast; do
+  rem=$(( $countlines % 2 ))
+
+  if [ $rem -eq 1 ]; then
+    podname=$podcast
+    echo -e "${YELLOW}$podname${NC}"
+
+  else
+    file=$(xsltproc parse_enclosure2.xsl $podcast 2> /dev/null || wget -q $podcast -O - | tr '\r' '\n' | tr \' \" | sed -n 's/.*url="\([^"]*\)".*/\1/p')
+
+    for url in $file; do
+      echo $url >> temp_pc.log
+      if [[ "$2" != "no-download" ]]; then
+        if ! grep "$url" podcast.log > /dev/null; then
+
+      	  if [[ "$2" == "list" ]]; then
+            echo $url >> $datadir$extm3u
+          fi
+
+  	  if [[ "$2" != "list" ]]; then
+            wget -q "$url" -O audio.mp3
+            poddate=$(stat -c "%y" "audio.mp3"|awk '{print $1"_"$2}'|sed 's/\..*$//'|sed 's/:/-/'|sed 's/:/-/')
+            newname=$podname$seprdr$poddate$extmp3
+            n=1
+
+            while [[ -f $(pwd)/$datadir/$newname ]]; do
+              newname=$podname$seprdr$poddate$n$extmp3
+              n=$(expr $n + 1)
+            done
+
+            if [ \! -s audio.mp3 ];then
+              rm "$(pwd)/audio.mp3"
+            else
+              echo -e "${GREEN}\t$newname${NC}"
+              mv "$(pwd)/audio.mp3" "$(pwd)/$datadir/$newname"
+            fi
+	    
+          fi
+	
+        fi
+      fi
+    done
+  fi
+  countlines=$(expr $countlines + 1)
+done < $1
+
+# If we just want a list
+if [ "$2" = "list" ]; then
+rmdir "$(pwd)/$datadir/"
+fi
 
 # Move dynamically created log file to permanent log file:
 cat podcast.log >> temp_pc.log
